ae827a144bf208726cc6149ff0f1e2d814705ac5
Valérian Galliat <vava720@gmail.com>
2014-05-19 15:36:36 +0200

    Added PHP Twig package and link Sublime Text preferences instead of copying.

--- src/sublime-text/install.sh
+++ src/sublime-text/install.sh
@@ -1,6 +1,7 @@
-link AsciiDoc .config/sublime-text-3/Packages/AsciiDoc
-link DocBlockr .config/sublime-text-3/Packages/DocBlockr
 link "Package Control" ".config/sublime-text-3/Packages/Package Control"
 link "Theme - Spacegray" ".config/sublime-text-3/Packages/Theme - Spacegray"
+link AsciiDoc .config/sublime-text-3/Packages/AsciiDoc
+link DocBlockr .config/sublime-text-3/Packages/DocBlockr
+link PHP-Twig .config/sublime-text-3/Packages/PHP-Twig
 
-copy User/Preferences.sublime-settings .config/sublime-text-3/Packages/User/Preferences.sublime-settings
+link User/Preferences.sublime-settings .config/sublime-text-3/Packages/User/Preferences.sublime-settings
