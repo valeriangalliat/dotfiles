f9a2ed76dc279046ad00e2ed2cca3c91984ab123
Valérian Galliat <vava720@gmail.com>
2014-05-19 15:42:53 +0200

    Added language-specific config.

--- src/sublime-text/install.sh
+++ src/sublime-text/install.sh
@@ -1,7 +1,14 @@
-link "Package Control" ".config/sublime-text-3/Packages/Package Control"
-link "Theme - Spacegray" ".config/sublime-text-3/Packages/Theme - Spacegray"
+link 'Package Control' '.config/sublime-text-3/Packages/Package Control'
+link 'Theme - Spacegray' '.config/sublime-text-3/Packages/Theme - Spacegray'
 link AsciiDoc .config/sublime-text-3/Packages/AsciiDoc
 link DocBlockr .config/sublime-text-3/Packages/DocBlockr
 link PHP-Twig .config/sublime-text-3/Packages/PHP-Twig
 
-link User/Preferences.sublime-settings .config/sublime-text-3/Packages/User/Preferences.sublime-settings
+setting() {
+    link "User/$1.sublime-settings" ".config/sublime-text-3/Packages/User/$1.sublime-settings"
+}
+
+setting Preferences
+setting HTML
+setting 'HTML (Twig)'
+setting XML
