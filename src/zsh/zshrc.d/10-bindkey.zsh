3bcecafd26c8310b4d7942e08134cad08142875d
Valérian Galliat <vava720@gmail.com>
2014-08-01 21:49:45 +0200

    Search from beginning of line.

--- src/zsh/zshrc.d/10-bindkey.zsh
+++ src/zsh/zshrc.d/10-bindkey.zsh
@@ -7,20 +7,20 @@ bindkey -v '^Z' undo
 bindkey -v '^R' redo
 
 #
+# Standard Bindings
+# =================
+#
 # More standard behavior even in Vi mode.
 #
 # See <https://wiki.archlinux.org/index.php/Zsh#Key_Bindings>.
 #
-# Note: I changed the up/down binding to also search the command beginning
-# in history if something is already typed. If the line is empty, it will
-# just move one line in history.
+# The up/down bindings are a little special, look below for this.
 #
+
 bindkey "${terminfo[khome]}" beginning-of-line # Home
 bindkey "${terminfo[kend]}" end-of-line # End
 bindkey "${terminfo[kich1]}" overwrite-mode # Insert
 bindkey "${terminfo[kdch1]}" delete-char # Delete
-bindkey "${terminfo[kcuu1]}" up-line-or-search # Up
-bindkey "${terminfo[kcud1]}" down-line-or-search # Down
 bindkey "${terminfo[kcub1]}" backward-char # Left
 bindkey "${terminfo[kcuf1]}" forward-char # Right
 bindkey "${terminfo[kpp]}" beginning-of-buffer-or-history # PageUp
@@ -29,3 +29,39 @@ bindkey "${terminfo[knp]}" end-of-buffer-or-history # PageDown
 # Emacs backward/forward word
 bindkey '\eOd' backward-word # Control+Left
 bindkey '\eOc' forward-word # Control+Right
+
+#
+# Move or Search
+# ==============
+#
+# Bind up/down keys to search the command beginning in history if
+# something is typed, else just move one line in history.
+#
+# This needs to autoload specific functions and to create user-defined
+# widgets with `zle -N`.
+#
+# See zshzle(1).
+#
+# I've made custom functions to wrap the original widgets to call
+# `forward-char` after the actual command, just to trigger an event
+# supported by the syntax highlighting function.
+#
+
+autoload up-line-or-beginning-search
+autoload down-line-or-beginning-search
+
+up-line-or-beginning-search-color() {
+    up-line-or-beginning-search
+    forward-char
+}
+
+down-line-or-beginning-search-color() {
+    down-line-or-beginning-search
+    forward-char
+}
+
+zle -N up-line-or-beginning-search-color
+zle -N down-line-or-beginning-search-color
+
+bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search-color # Up
+bindkey "${terminfo[kcud1]}" down-line-or-beginning-search-color # Down
