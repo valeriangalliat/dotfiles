3b550d96b560fc67f878735d95dd382a4098dbc9
Valérian Galliat <vava720@gmail.com>
2014-11-09 18:29:39 +0100

    Drastically simplify dependencies managment script

---
+++ deps/deps.sh
@@ -0,0 +1,42 @@
+cat=misc
+    dep=noelup
+    github valeriangalliat/noelup
+
+    dep=printscreen
+    github valeriangalliat/printscreen
+
+    dep=block
+    github valeriangalliat/block
+
+    dep=vimpager
+    github rkitover/vimpager
+
+cat=zsh
+    dep=ls-colors
+    github trapd00r/LS_COLORS
+
+    dep=syntax-highlighting
+    github trapd00r/zsh-syntax-highlighting-filetypes
+
+    dep=jvc-tools
+    github valeriangalliat/jvc-tools
+
+cat=urxvt
+    dep=perls
+    github muennich/urxvt-perls
+
+cat=sublime-text
+    dep=package-control
+    github wbond/sublime_package_control
+
+    dep=spacegray
+    github kkga/spacegray
+
+    dep=jsdocs
+    github spadgos/sublime-jsdocs
+
+    dep=asciidoc
+    github SublimeText/AsciiDoc
+
+    dep=twig
+    github Anomareh/PHP-Twig.tmbundle
