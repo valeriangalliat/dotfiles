69fa2a81f8fff765adad7dcb8854d2fc97d8a802
Valérian Galliat <val@codejam.info>
2016-04-10 17:14:13 -0400

    Refactor Vim configuration

---
+++ src/vim/plugins.sh
@@ -0,0 +1,244 @@
+# Essentials {{{
+# ==============
+
+    # Defaults everyone can agree on
+    gh tpope/vim-sensible
+
+    # Fancy status line
+    gh vim-airline/vim-airline
+    gh vim-airline/vim-airline-themes
+
+    # Buffers in status line
+    # vim-bufferline
+
+    # tmux integration
+    gh christoomey/vim-tmux-navigator
+
+    # Proper buffer closing
+    gh moll/vim-bbye
+
+    # Better split explorer
+    gh tpope/vim-vinegar
+
+# }}}
+
+
+# Finder, fuzzy, menu {{{
+# =======================
+
+    gh junegunn/fzf
+
+    # Find anything
+    # gh Shougo/unite.vim
+
+    # Fuzzy finder (faster than Unite equivalent)
+    # gh kien/ctrlp.vim
+
+    # Fuzzy finder (needs Ruby)
+    # gh wincent/command-t
+
+# }}}
+
+# Autocompletion {{{
+# ==================
+
+    # Browse completion with `<Tab>`
+    # gh ervandew/supertab
+
+    # Autocompletion (lightweight)
+    # gh vim-scripts/AutoComplPop
+
+    # Autocompletion (intermediate)
+    gh Shougo/neocomplcache.vim
+
+    # Autocompletion (same as `neocomplcache`, faster, but needs Lua)
+    # gh Shougo/neocomplete.vim
+
+    # Autocompletion (heavy and powerful)
+    # gh Valloric/YouCompleteMe
+
+# }}}
+
+# Snippets {{{
+# ============
+
+    # Actual snippets
+    # gh honza/vim-snippets
+
+    # Snippets engine
+    # gh SirVer/ultisnips
+
+    # Actual snippets (alternative)
+    # gh Shougo/neosnippet-snippets
+
+    # Snippets engine (alternative)
+    # gh Shougo/neosnippet.vim
+
+# }}}
+
+# Theme {{{
+# =========
+
+    # Color scheme
+    gh chriskempson/base16-vim
+
+# }}}
+
+# Misc {{{
+# ========
+
+    # Repeat with `.` for plugins
+    gh tpope/vim-repeat
+
+    # Multiple cursors (kinda) like Sublime Text
+    # gh terryma/vim-multiple-cursors
+
+# }}}
+
+# Motions/objects {{{
+# ===================
+
+    # New text objects
+    gh wellle/targets.vim
+
+    # Switch arguments
+    # gh PeterRincker/vim-argumentative
+
+    # Quick motions
+    gh easymotion/vim-easymotion
+
+    # Exchange operator
+    gh tommcdo/vim-exchange
+
+    # Change surroundings
+    gh tpope/vim-surround
+
+    # Select by indent level
+    gh qstrahl/vim-dentures
+
+    # Alignment helper
+    # gh tommcdo/vim-lion
+
+    # Camel case and underscore motions
+    gh bkad/CamelCaseMotion
+
+# }}}
+
+# Syntax {{{
+# ==========
+
+    # gh tpope/vim-git
+    gh pangloss/vim-javascript
+    gh godlygeek/tabular # Needed by `vim-markdown`
+    gh plasticboy/vim-markdown
+    # gh bohrshaw/vim-vimperator-syntax
+    # gh digitaltoad/vim-jade
+    # gh wavded/vim-stylus
+    gh othree/html5.vim
+    gh elzr/vim-json
+    gh rust-lang/rust.vim
+    # gh chrisbra/csv.vim
+    # gh PotatoesMaster/i3-vim-syntax
+    # gh kchmck/vim-coffee-script
+    gh LnL7/vim-nix
+
+# }}}
+
+# Auto {{{
+# ========
+
+    # Auto close blocks
+    gh tpope/vim-endwise
+
+    # Auto close structures and quotes (awesome fork to avoid annoying mode)
+    gh jiangmiao/auto-pairs
+
+    # Auto toggle paste mode
+    gh ConradIrwin/vim-bracketed-paste
+
+    # Auto change to project root
+    # gh airblade/vim-rooter
+
+    # Auto detect indent (disabled because not accurate)
+    # gh ciaranm/detectindent
+
+# }}}
+
+# Helpers {{{
+# ===========
+
+    # Git integration
+    # gh tpope/vim-fugitive
+
+    # Show modified lines in Git projects
+    # gh airblade/vim-gitgutter
+
+    # Clipboard helper
+    gh svermeulen/vim-easyclip
+
+    # Comment stuff out
+    gh tpope/vim-commentary
+
+    # Narrow region to focus edit
+    gh chrisbra/NrrwRgn
+
+    # Use ack with Vim
+    # gh mileszs/ack.vim
+
+    # Advanced search and replace
+    # gh dyng/ctrlsf.vim
+
+    # Advanced search and replace (change in place)
+    # gh AndrewRadev/writable_search.vim
+
+    # HTML shortcuts
+    gh mattn/emmet-vim
+
+    # `%` for HTML, LaTeX and more (native in recent Vim versions)
+    # gh vim-scripts/matchit.zip
+
+    # Syntax checker
+    # gh scrooloose/syntastic
+
+    # Switch expressions (true/false, on/off, etc.)
+    gh AndrewRadev/switch.vim
+
+    # Manage GitHub gists
+    gh mattn/gist-vim
+
+    # Upload on Deblan wall
+    gh valeriangalliat/dwall
+
+    # Switch between single-line and multiline statements
+    # gh AndrewRadev/splitjoin.vim
+
+    # Switch between single-line and multiple statements (alternative)
+    # gh FooSoft/vim-argwrap
+
+# }}}
+
+# UI {{{
+# ======
+
+    # Source explorer
+    # gh wesleyche/SrcExpl
+
+    # Browse undo tree
+    gh mbbill/undotree
+
+    # Show marks
+    # gh kshenoy/vim-signature
+
+    # Show tags from file
+    gh majutsushi/tagbar
+
+    # Context-aware relative or absolute numbers
+    # gh myusuf3/numbers.vim
+
+    # Mark indent (disabled because slow)
+    # gh Yggdroot/indentLine
+
+    # Distraction-free mode
+    gh junegunn/goyo.vim
+
+# }}}
