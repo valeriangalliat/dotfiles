e505c595e3c259fa68da990b5e48ca81815bb558
Valérian Galliat <val@codejam.info>
2018-02-11 13:55:28 -0500

    Tweak deps

--- deps/deps.sh
+++ deps/deps.sh
@@ -1,56 +1,36 @@
-cat=git
-    dep=git-extras
-    github tj/git-extras
-
 cat=misc
     dep=noelup
-    github valeriangalliat/noelup
+    gh valeriangalliat/noelup
 
     dep=printscreen
-    github valeriangalliat/printscreen
+    gh valeriangalliat/printscreen
 
     dep=block
-    github valeriangalliat/block
+    gh valeriangalliat/block
 
     dep=jvc-tools
-    github valeriangalliat/jvc-tools
+    gh valeriangalliat/jvc-tools
 
 cat=shell
     dep=base16-shell
-    github chriskempson/base16-shell
+    gh chriskempson/base16-shell
 
     dep=vimpager
-    github rkitover/vimpager
+    gh rkitover/vimpager
 
-    dep=vimsed
-    github ryanfmurphy/vimsed
+    # dep=vimsed
+    # gh ryanfmurphy/vimsed
 
-    dep=fzf
-    github junegunn/fzf
-
-cat=sublime-text
-    dep=package-control
-    github wbond/sublime_package_control
-
-    dep=spacegray
-    github kkga/spacegray
-
-    dep=jsdocs
-    github spadgos/sublime-jsdocs
-
-    dep=asciidoc
-    github SublimeText/AsciiDoc
-
-    dep=twig
-    github Anomareh/PHP-Twig.tmbundle
+    # dep=fzf
+    # gh junegunn/fzf
 
 cat=urxvt
     dep=perls
-    github muennich/urxvt-perls
+    gh muennich/urxvt-perls
 
 cat=zsh
     dep=oh-my-zsh
-    github robbyrussell/oh-my-zsh
+    gh robbyrussell/oh-my-zsh
 
     dep=zsh-syntax-highlighting
-    github zsh-users/zsh-syntax-highlighting
+    gh zsh-users/zsh-syntax-highlighting
