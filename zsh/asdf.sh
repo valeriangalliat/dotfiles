1896ec97fe0fe5efcadd4345d3cbd288cbe43b5d
Val <val@codejam.info>
2022-05-09 19:39:26 -0400

    Add asdf-use

--- zsh/asdf.sh
+++ zsh/asdf.sh
@@ -1,7 +1,9 @@
 # asdf hack to make it faster for the global path
 asdf() {
     [ -d ~/.asdf ] || git clone https://github.com/asdf-vm/asdf ~/.asdf
+    [ -d ~/.asdf/asdf-use ] || git clone https://github.com/valeriangalliat/asdf-use ~/.asdf/asdf-use
     . ~/.asdf/asdf.sh
+    . ~/.asdf/asdf-use/asdf-use.sh
 
     if [ "$#" -gt 0 ]; then
         asdf "$@"
