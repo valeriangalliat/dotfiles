e58b97f8d06cc3091283d54699fe44ece4a53d6d
Val <val@codejam.info>
2026-08-01 17:02:29 -0700

    Mac defaults tweaks

--- mac/defaults.sh
+++ mac/defaults.sh
@@ -1,37 +1,43 @@
 #!/bin/sh -e
 
+# Clean up Dock defaults
 defaults write com.apple.dock persistent-apps -array
 defaults write com.apple.dock persistent-others -array
 
+# Custom screenshots directory
 mkdir -p ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
 
+# General > Date & Time: 24-hour time
 defaults write -g AppleICUForce24HourTime -bool true
 
+# Desktop & Dock > Dock: Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Desktop & Dock > Dock: Don't show suggested and recent apps
 defaults write com.apple.dock show-recents -bool false
+
+# Desktop & Dock > Dock: Remove the delay to show and hide the dock
 defaults write com.apple.dock autohide-delay -float 0
 
+# Desktop & Dock > Windows: Tiled windows have margins
 defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
 
+# Desktop & Dock > Mission Control: Don't automatically rearrange Spaces
 defaults write com.apple.dock mru-spaces -bool false
 
+# Desktop & Dock > Hot Corners (disable all, bottom right is Quick Note by default)
 defaults write com.apple.dock wvous-tl-corner -int 0
 defaults write com.apple.dock wvous-tr-corner -int 0
 defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-br-corner -int 0
 
-defaults write -g KeyRepeat -int 1
+# Keyboard: Key repeat rate & Delay until repeat (fastest possible, I like a snappy keyboard)
+defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write com.microsoft.VSCode AppleWindowTabbingMode -string always
-
+# Cursor
 defaults write com.todesktop.230313mzl4w4u92 ApplePressAndHoldEnabled -bool false
 defaults write com.todesktop.230313mzl4w4u92 AppleWindowTabbingMode -string always
 
-defaults write com.google.antigravity ApplePressAndHoldEnabled -bool false
-defaults write com.google.antigravity AppleWindowTabbingMode -string always
-
-killall Dock 2>/dev/null || true
-killall SystemUIServer 2>/dev/null || true
+echo "Settings configured, log out and back in to fully take effect"
